3958e26fad9dda144e79a6f8c6edcd344fa21391
Slavik <miska.cypa@gmail.com>
2023-02-16 19:14:28 +1000

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"X = {self.x} and Y = {self.y}"
-
-print(Point(10, -2))
