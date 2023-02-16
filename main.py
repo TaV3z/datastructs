db7060645ce230e7635aede11145748d16c8502b
Slavik <miska.cypa@gmail.com>
2023-02-16 19:21:28 +1000

    added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from linked_list.singly_linked_list import SinglyLinkedList as SingList
+
+
+if __name__ == '__main__':
+    sl_list = SingList()
+
+    sl_list.push_back(10)
+    sl_list.push_back(20)
+    sl_list.push_back(30)
+
+    print(sl_list)
